84299f5c04e7d191f86f7363238211fbddeb6ff0
shankarrathode <shankarrathode09@gmail.com>
2025-03-15 13:49:19 +0530

    Fixed port issue

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,5 @@ def home():
     return "Hello, Flask on Render!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))  # Render assigns a port dynamically
+    app.run(host='0.0.0.0', port=port)
